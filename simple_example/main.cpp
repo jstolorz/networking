02c953a7e8441519b2730ddfaf4cabbc5056fec3
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-01-12 17:35:43 +0100

    asio library setup

--- simple_example/main.cpp
+++ simple_example/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+
+#define ASIO_STANDALONE
 #include <asio.hpp>
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
 int main(){
+
+    asio::error_code ec;
+
+
+
     return 0;
 }
 
