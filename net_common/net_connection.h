74e680b2b3122ba411f6ee962e7bd05fed0074f9
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-01-17 17:34:42 +0100

    adding part of server interface

--- net_common/net_connection.h
+++ net_common/net_connection.h
@@ -10,11 +10,40 @@ namespace bluesoft{
         template<typename T>
         class connection : public  std::enable_shared_from_this<connection<T>>{
         public:
-            connection(){}
+
+            enum class owner{
+                server,
+                client
+            };
+
+            connection(owner parent, asio::io_context& asio_context,
+                       asio::ip::tcp::socket socket,
+                       tsqueue<owned_message<T>>& q_in)
+                       : m_asio_context(asio_context), m_socket(std::move(socket)),m_qmessage_in(q_in)
+            {
+                m_nowner_type = parent;
+            }
+
             virtual ~connection(){}
 
+            // This ID is used system wide - its how clients will understand other clients
+            // exist across the whole system.
+            uint32_t get_id() const
+            {
+                return id;
+            }
+
         public:
-            bool connect_to_server();
+
+            void connect_to_client(uint32_t uid = 0){
+                if(m_nowner_type == owner::server){
+                    if(m_socket.is_open()){
+                        id = uid;
+                    }
+                }
+            }
+
+            void connect_to_server(const asio::ip::tcp::resolver::results_type& endpoints);
             bool disconnect();
             bool is_connected() const;
 
@@ -37,6 +66,10 @@ namespace bluesoft{
             // as the owner of this connection is expected to provide a queue
             tsqueue<owned_message<T>>& m_qmessage_in;
 
+            // The "owner" decides how some of the connection behaves
+            owner m_nowner_type = owner::server;
+
+            uint32_t id = 0;
         };
     }
 }
--- net_common/net_server.h
+++ net_common/net_server.h
@@ -1,5 +1,170 @@
-//
-// Created by janusz on 15.01.2022.
-//
-
 #pragma once
+
+
+#include "net_common.h"
+#include "net_message.h"
+#include "net_tsqueue.h"
+#include "net_connection.h"
+
+namespace bluesoft{
+    namespace net{
+        template<typename T>
+        class server_interface{
+        public:
+
+            server_interface(uint16_t port)
+            : m_acceptor(m_io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port))
+            {}
+
+            virtual ~server_interface(){
+               stop();
+            }
+
+            bool start(){
+               try {
+
+                   wait_for_client_connection();
+
+                   m_thread_context = std::thread([this](){ m_io_context.run(); });
+
+               }catch (std::exception& ex){
+                   std::cerr <<"[SERVER] Exception: " << ex.what() << "\n";
+                   return false;
+               }
+
+               std::cout << "[SERVER] Started... \n";
+                return true;
+            }
+
+            void stop(){
+                // Request the context to close
+                m_io_context.stop();
+
+                // Tidy up the context thread
+                if(m_thread_context.joinable()){
+                    m_thread_context.join();
+                }
+
+                // Inform someone, anybody, if they care...
+                std::cout <<"[SERVER] Stopped...\n";
+            }
+
+            // ASYNC - Instruct asio to wait for connection
+            void wait_for_client_connection(){
+                 m_acceptor.template async_accept(
+                         [this](std::error_code ec, asio::ip::tcp::socket socket){
+                             if(!ec){
+                                std::cout <<"[SERVER] New Connection: " << socket.remote_endpoint() << "\n";
+
+                                std::shared_ptr<connection<T>> newconn =
+                                        std::make_shared<connection<T>>(connection<T>::owner::server,
+                                                m_io_context, std::move(socket), m_qmessage_in
+                                                );
+
+                                // Give the user server a chance to deny connection
+                                if(on_client_connect(newconn)){
+                                    // Connection allowed, so add to container of new connection
+                                    m_deq_connections.push_back(std::move(newconn));
+                                    m_deq_connections.back()->connect_to_client(n_ID_counter++);
+                                    std::cout <<"[" << m_deq_connections.back()->get_id() << "] Connection Approved\n";
+                                }else{
+                                    std::cout << "[------] Connection Denied\n";
+                                }
+
+                             } else{
+                                 // Error has occurred during acceptance
+                                 std::cout << "[SERVER] New Connection Error: " << ec.message() << "\n";
+                             }
+                             // Prime the asio context with more work - again simple wait for
+                             // another connection...
+                             wait_for_client_connection();
+                         });
+            }
+
+            // Send a message to a specific client
+            void message_client(std::shared_ptr<connection<T>> client, const message<T>& msg){
+                 if(client && client->is_connected()){
+                     client->send(msg);
+                 }else{
+                     on_client_disconnect(client);
+                     client.reset();
+                     // Then physically remove it from the container
+                     m_deq_connections.erase(
+                             std::remove(m_deq_connections.begin(), m_deq_connections.end(), client), m_deq_connections.end());
+                 }
+            }
+
+            // Send message to all clients
+            void message_all_client(const message<T> msg, std::shared_ptr<connection<T>> p_ignore_client = nullptr){
+
+                 bool b_invalid_client_exists = false;
+
+                 for(auto& client : m_deq_connections){
+                     // Check client is connected...
+                     if(client && client->is_connected()){
+                         if(client != p_ignore_client){
+                             client->send(msg);
+                         }
+                     }else{
+                         on_client_disconnect(client);
+                         client.reset();
+                         b_invalid_client_exists = true;
+                     }
+                 }
+
+                // Remove dead clients, all in one go - this way, we dont invalidate the
+                // container as we iterated through it.
+                if (b_invalid_client_exists)
+                    m_deq_connections.erase(
+                            std::remove(m_deq_connections.begin(), m_deq_connections.end(), nullptr), m_deq_connections.end());
+            }
+
+            void update(size_t n_max_message = -1){
+                size_t  n_message_count = 0;
+                while (n_message_count < n_max_message && !m_qmessage_in.empty()){
+                    // Grab the front message
+                    auto msg = m_qmessage_in.pop_front();
+
+                    // Pass to message handler
+                    on_message(msg.remote, msg.msg);
+                    n_message_count++;
+                }
+            }
+
+        protected:
+
+            // Called when a client connects, you can veto the connection by returning false
+            virtual bool on_client_connect(std::shared_ptr<connection<T>> client){
+                return false;
+            }
+
+            // Called when a client appears to have disconnected
+            virtual void on_client_disconnect(std::shared_ptr<connection<T>> client){
+
+            }
+
+            // Called when message arives
+            virtual void on_message(std::shared_ptr<connection<T>> client, message<T> msg){
+
+            }
+
+        protected:
+
+            // Thread safe queue for incoming message packets
+            tsqueue<owned_message<T>> m_qmessage_in;
+
+            // container of active validated connections
+            std::deque<std::shared_ptr<connection<T>>> m_deq_connections;
+
+            // Order of declaration is important - it is also the order of initialisation
+            asio::io_context m_io_context;
+            std::thread m_thread_context;
+
+            // This things need an asio context
+            asio::ip::tcp::acceptor m_acceptor;
+
+            // Client will be identified in the "wider system" via an ID
+            uint32_t n_ID_counter = 10000;
+        };
+    }
+}
